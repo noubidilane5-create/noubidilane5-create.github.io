d02af72f1aaf8581f29bd9094166a331dad8b50e
Noubi Steve <noubidilane5@gmail.com>
2026-08-18 08:19:41 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: { display: ['PT Sans', 'sans-serif'], body: ['DM Sans', 'sans-serif'] },
+      colors: { accent: '#FF6B2B', 'accent-light': '#FF8F5C' }
+    }
+  },
+  plugins: []
+}
